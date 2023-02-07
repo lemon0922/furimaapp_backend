e4cf336ce67b6a79e2d1b03f02227030852311ea
imado0922 <shohei.imado@divx.co.jp>
2023-02-07 13:26:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :burden_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :scheduled_data_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price,
+            { format: { with: /\A[0-9]+\z/ }
+              numericality: { only_integer: true, greater_than_or_equel_to: 300, less_than_or_equel_to: 9_999_999 } }
+  validates :image, presence: true
 end
