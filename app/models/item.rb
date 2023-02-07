04c0852cb8102a3a39c55de95965aa21db026182
imado0922 <shohei.imado@divx.co.jp>
2023-02-07 14:47:15 +0900

    モデル間のアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,13 @@ class Item < ApplicationRecord
             { format: { with: /\A[0-9]+\z/ }
               numericality: { only_integer: true, greater_than_or_equel_to: 300, less_than_or_equel_to: 9_999_999 } }
   validates :image,             presence: true
+
+  has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :scheduled_date
 end
