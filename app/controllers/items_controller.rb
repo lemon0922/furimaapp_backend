cda877e9de3fcac0971f8fea11fee0a50cd2d990
imado0922 <shohei.imado@divx.co.jp>
2023-02-09 13:32:11 +0900

    seedに初期データを用意

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).pemit(:title, :content, :category_id, :condition_id, :burden_id, :area_id, :scheduled_id, :price, :image)
   end
 end
